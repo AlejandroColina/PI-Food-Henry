1f4a9e4ce1b3063749e7f65e96c982a155962fb4
AlejandroColina <alejo_9102@hotmail.com>
2022-04-25 12:09:01 -0500

    'testing'

---
+++ api/src/axios/index.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+async function data() {
+    try {
+        const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=4319acd0dbd04057a612db7cd4e531c5&addRecipeInformation=true&number=1');
+        // if (response.ok != 200) throw new Error(`Error HTTP: ${response.status}`);
+        let data = await response.data.results
+        return data;
+    } catch (error) {
+        console.error(`Could not get products ${error}`)
+    }
+};
+
+module.exports = data;
